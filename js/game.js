0e8f934ee5d78f4cceea2361b7732cda3884a513
Dylan Ng <ngxinde@gmail.com>
2016-07-12 17:34:00 +0800

    added movement, collision detection and bomber 2

--- js/game.js
+++ js/game.js
@@ -1,44 +1,46 @@
 // Create scope to nest all variables in
-(function(isometric) {
+(function(gameEngine) {
 
-  // Two Dimensional Array storing our isometric map layout. Each number represents a tile.
+  // Two Dimensional Array storing our world map layout. Each number represents a tile.
   var map = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], //top of canvas
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    //top of canvas
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], //bottom of canvas
   ];
 
   var tileImages = [];
   var bomber1X = 1,
-      bomber1Y = 1;
+    bomber1Y = 1,
+    bomber2X = 23,
+    bomber2Y = 13;
 
   function loadImg() {
 
     // Images to be loaded and used.
     // Tutorial Note: As water is loaded first it will be represented by a 0 on the map and land will be a 1.
-    var landTiles = ["./images/sand.png", "./images/metalwall.png", "./images/bombertest.png"],
-        //bombers = ["./images/bomber.png", "./images/bomber2.png"],
-        totalImgsLoaded = 0;
+    var imagesArray = ["./images/sand.png", "./images/metalwall.png", "./images/bomber1.png", "./images/bomber2.png"],
+      totalImgsLoaded = 0;
 
-    for (var i = 0; i < landTiles.length; i++) {
+    for (var i = 0; i < imagesArray.length; i++) {
       tileImages[i] = new Image();
-      tileImages[i].src = landTiles[i];
+      tileImages[i].src = imagesArray[i];
       tileImages[i].onload = function() {
         // Once the image is loaded increment the loaded graphics count and check if all images are ready.
         totalImgsLoaded++;
-        if (totalImgsLoaded === landTiles.length) {
+        if (totalImgsLoaded === imagesArray.length) {
           drawMap();
         }
       };
@@ -68,42 +70,113 @@
         drawTile = map[i][j];
         // Draw the represented image number, at the desired X & Y coordinates followed by the graphic width and height.
         ctx.drawImage(tileImages[drawTile], (j * tileH) + mapX, (i * tileH) + mapY);
-        //draw bomber 1
-        if (bomber1X === i && bomber1Y === j) {
-          ctx.drawImage(tileImages[2], (j * tileH) + mapX, (i * tileH) + mapY);
+        //draw bomber 1, j= column, i= row
+        if (bomber1X === j && bomber1Y === i) {
+          ctx.drawImage(tileImages[2], (j * tileW) + mapX, (i * tileH) + mapY);
+        } else if (bomber2X == j && bomber2Y == i) {
+          ctx.drawImage(tileImages[3], (j * tileW) + mapX, (i * tileH) + mapY);
         }
       }
     }
   }
 
+  function checkCollision() {
+
+    function checkP1() {
+      if (bomber1X % 2 === 0 && bomber1Y % 2 === 0 ||
+        bomber1X === 0 || bomber1X == 24 || bomber1Y === 0 || bomber1Y == 14) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    function checkP2() {
+      if (bomber2X % 2 === 0 && bomber2Y % 2 === 0 ||
+        bomber2X === 0 || bomber2X == 24 || bomber2Y === 0 || bomber2Y == 14) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    if(checkP1() === true || checkP2() === true) {
+      return true;
+    }
+    else {
+      return false;
+    }
+
+  }
+
   function init() {
     // Remove Event Listener and load images.
-    isometric.removeEventListener('load', init);
+    gameEngine.removeEventListener('load', init);
     loadImg();
-    //add keys Listener
-    isometric.addEventListener("keydown", function(e) {
-      console.log(e.keyCode);
-      switch(e.keyCode) {
-        case 37:  //left
+    //add p1keys Listener
+    gameEngine.addEventListener("keydown", function(e) {
+
+      switch (e.keyCode) {
+        //player 1 keys
+        case 65: //a
           bomber1X--;
-        break;
-        case 39:  //right
+          if (checkCollision() === true) {
+            bomber1X++;
+          }
+          break;
+        case 68: //d
           bomber1X++;
-        break;
-        case 38:  //top
+          if (checkCollision() === true) {
+            bomber1X--;
+          }
+          break;
+        case 87: //w
           bomber1Y--;
-        break;
-        case 40:  //down
+          if (checkCollision() === true) {
+            bomber1Y++;
+          }
+          break;
+        case 83: //s
           bomber1Y++;
-        break;
+          if (checkCollision() === true) {
+            bomber1Y--;
+          }
+          break;
+          //player 2 keys
+        case 37: //left
+          bomber2X--;
+          if (checkCollision() === true) {
+            bomber2X++;
+          }
+          break;
+        case 39: //right
+          bomber2X++;
+          if (checkCollision() === true) {
+            bomber2X--;
+          }
+          break;
+        case 38: //up
+          bomber2Y--;
+          if (checkCollision() === true) {
+            bomber2Y++;
+          }
+          break;
+        case 40: //down
+          bomber2Y++;
+          if (checkCollision() === true) {
+            bomber2Y--;
+          }
       }
+      //console.log(bomber1X, bomber1Y);
       drawMap();
     });
+
+
   }
 
 
   // Add Event Listener to detect when page has fully loaded.
-  isometric.addEventListener('load', init, false);
+  gameEngine.addEventListener('load', init, false);
 
 })(this);
 
